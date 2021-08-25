b2b0c78ab2cf7309464088c0ba33dddfb90ff83f
ferdous85 <ferdous85@gmail.com>
2021-08-25 22:04:11 +0600

    style-added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
-import React from 'react';
 import './App.css';
 
-function App() {
+const App: React.FC = ()=> {
   return (
     <div className="App">
-      <h1>Hello TSX</h1>
+      <span className="heading">Taskify</span> 
     </div>
   );
 }

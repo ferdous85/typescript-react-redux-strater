4663bff407ad58608ef420722cc1ed15e27b3d68
ferdous85 <ferdous85@gmail.com>
2021-08-26 13:00:38 +0600

    style add

---
+++ src/components/InputField.tsx
@@ -0,0 +1,19 @@
+import './InputField.css'
+
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>
+}
+const InputField: React.FC <Props> = ({todo, setTodo}: Props) => {
+  return (
+    <form action="" className="input">
+      <input type="text" placeholder="Enter a task" className="input__box"
+      value={todo}
+      onChange={e=>setTodo(e.target.value)}
+      />
+      <button className="input_submit" type='submit'>Go</button>
+    </form>
+  )
+}
+
+export default InputField
